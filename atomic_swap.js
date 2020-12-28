71c43d9b82afc92a65533940343587c678eec5e3
Byron Appelt <byron.appelt@gmail.com>
2020-12-28 12:24:46 -0600

    remove remnants of cmd line interaction

--- atomic_swap.js
+++ atomic_swap.js
@@ -29,15 +29,6 @@ const AtomicSwapRedeemScript = require('./lib/AtomicSwapRedeemScript');
 const AtomicSwapRedeemTransaction = require('./lib/AtomicSwapRedeemTransaction');
 const PrivateKey = require('@dashevo/dashcore-lib/lib/privatekey');
 const AtomicSwapUnlockingScript = require('./lib/AtomicSwapUnlockingScript');
-var cl = readln.createInterface( process.stdin, process.stdout );
-
-var question = function(q) {
-    return new Promise( (res, rej) => {
-        cl.question( q, answer => {
-            res(answer);
-        })
-    });
-};
 
 (async function main() {
     var participantPubKeyString;
@@ -129,14 +120,6 @@ var question = function(q) {
     function fail5(error) {
         console.error("Public Key failed - " + error.error);
     }
-
-    //var participantPubKeyString = await question('\n\nParticipant Public Key? ');
-    //var participantPubKey = PublicKey(participantPubKeyString);
-    //var toAddress = await question("Participant Address? ");
-    //var privKeyString = await question("Participant private key? ");
-    //let privKey = PrivateKey.fromString(privKeyString);
-    //var initiatorPubKeyString = await question('Initiator Public Key? ');
-    //let initiatorPubKey = PublicKey(initiatorPubKeyString);
     
     var secret = getSecret();
     console.log(secret);
@@ -175,12 +158,6 @@ var question = function(q) {
         console.error("getrawtransaction failed - " + error.error);
     }
 
-    
-    // var fundingTxId = await question("\n\nFunding Transaction ID? ");
-    // var fundingTxOutputIdx = await question("Funding Transaction Index? ");
-    // var fundingTxScriptPubKeyString = await question("Funding Transaction script pubKey Hex? ");
-    // let fundingTxScriptPubKey = Script.fromHex(fundingTxScriptPubKeyString);
-
     var redeemTx = new AtomicSwapRedeemTransaction(
         swapAddress, fundingTxId, fundingTxOutputIdx, 1000000000-500, toAddress
     )
@@ -205,8 +182,6 @@ var question = function(q) {
     function fail8(error) {
         console.error("Sendrawtransaction failed - " + JSON.stringify(error));
     }
-    
-    cl.close();
 })();
 
 
